ffa2c33c762354aea7fc32b60c69336753aaa773
KhonsadiyaNasywaSalsabila <nasywa9905@gmail.com>
2024-05-31 11:20:26 +0700

    insert untuk pilihan pilihan yang dapat dipilih

--- CircularQueue/CircularQueue.cpp
+++ CircularQueue/CircularQueue.cpp
@@ -101,7 +101,20 @@ public:
 		Queues q;
 		char ch;
 
+		while (true) {
+			try {
+				cout << "Menu" << endl;
+				cout << "1. Implement Insert opertaion" << endl;
+				cout << "2. Implement Delete operation" << endl;
+				cout << "3. Display Values" << endl;
+				cout << "4. Exit" << endl;
+				cout << "Enter your choice (1-4): ";
+				cin >> ch;
+				cout << endl;
 
+
+			}
+		}
 	}
 
 };
